dc1e879b44232be6d4867b7ec4034ce93792015f
roochieng <49204011+roochieng@users.noreply.github.com>
2023-01-28 18:46:01 +0300

    adding code

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -1,3 +1,8 @@
 # Youtube Downloader project
 
 import requests
+
+from pytube import YouTube
+from pytube.streams import Stream
+
+YouTube("https://www.youtube.com/watch?v=DPnqb74Smug").streams.get_highest_resolution().download()
