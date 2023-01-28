60a189dd287edad8d502dff6d898956aad26b3d8
roochieng <49204011+roochieng@users.noreply.github.com>
2023-01-28 18:58:09 +0300

    Update Youtube_Downloader.py

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -6,3 +6,5 @@ from pytube import YouTube
 from pytube.streams import Stream
 
 YouTube("https://www.youtube.com/watch?v=DPnqb74Smug").streams.get_highest_resolution().download()
+
+# This should work
